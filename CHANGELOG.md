a61ea43a17913a65508cc7311b5f16a14bf9ee5a
yuchen <yuchenz27@outlook.com>
2023-08-10 12:33:15 +0800

    update to 0.3.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.3.3] - 2023.8.10
+
+### Added
+
+- Now you can add HoloKit XROrigin in the Unity Editor's Hierarchy Window
+
 ## [0.3.2] - 2023.7.23
 
 ### Added
---
+++ Editor/CreateUtils.cs
@@ -0,0 +1,68 @@
+// SPDX-FileCopyrightText: Copyright 2023 Holo Interactive <dev@holoi.com>
+// SPDX-FileCopyrightText: Copyright 2020 Unity Technologies ApS
+// SPDX-FileContributor: Yuchen Zhang <yuchen@holoi.com>
+// SPDX-License-Identifier: MIT AND LicenseRef-Unity-Companion-License
+
+using System;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEditor;
+
+namespace HoloInteractive.XR.HoloKit.Editor
+{
+    static class CreateUtils
+    {
+        /// <summary>
+        /// Makes <paramref name="gameObject"/> a child of <paramref name="parent"/> with no offset position or rotation.
+        /// If <paramref name="parent"/> is null, moves <paramref name="gameObject"/> to the scene pivot, or instead to
+        /// the world origin if there is also no Scene view.
+        /// </summary>
+        /// <param name="gameObject">The GameObject to place.</param>
+        /// <param name="parent">Optional GameObject to use as a parent of <paramref name="gameObject"/>.</param>
+        public static void Place(GameObject gameObject, Transform parent = null)
+        {
+            if (gameObject == null)
+            {
+                throw new ArgumentNullException(nameof(gameObject));
+            }
+
+            var transform = gameObject.transform;
+
+            if (parent != null)
+            {
+                ResetTransform(transform);
+                Undo.SetTransformParent(transform, parent, "Reparenting");
+                ResetTransform(transform);
+                gameObject.layer = parent.gameObject.layer;
+            }
+            else
+            {
+                // Puts it at the scene pivot, and otherwise world origin if there is no Scene view.
+                var view = SceneView.lastActiveSceneView;
+                if (view != null)
+                    view.MoveToView(transform);
+                else
+                    transform.position = Vector3.zero;
+
+                StageUtility.PlaceGameObjectInCurrentStage(gameObject);
+            }
+
+            GameObjectUtility.EnsureUniqueNameForSibling(gameObject);
+        }
+
+        static void ResetTransform(Transform transform)
+        {
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+            transform.localScale = Vector3.one;
+
+            if (transform is RectTransform rectTransform)
+            {
+                rectTransform.anchorMin = Vector2.zero;
+                rectTransform.anchorMax = Vector2.one;
+                rectTransform.anchoredPosition = Vector2.zero;
+                rectTransform.sizeDelta = Vector2.zero;
+            }
+        }
+    }
+}
